644f6a076a033f850f4d567a43af83eb1c153e4d
arskdj <68353557+arskdj@users.noreply.github.com>
2020-12-02 16:04:11 +0200

    use regex

--- day2.js
+++ day2.js
@@ -1,45 +1,39 @@
 const fs = require("fs");
 
 fs.readFile("./day2.input", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+    if (err) {
+        console.error(err);
+        return;
+    }
 
-  const lines = data.trim().split("\n");
+    const lines = data.trim().split("\n");
 
-  part1(lines);
-  part2(lines);
+    part1(lines);
+    part2(lines);
 });
 
 function parse_line(line) {
-  const regex = /(\d+)-(\d+) (\w): (\w+)$/;
-  return line.match(regex);
+    const regex = /(\d+)-(\d+) (\w): (\w+)$/;
+    return line.match(regex);
 }
 
 function validate_password(input) {
-  const [_, a, b, letter, password] = parse_line(input);
-  const letter_count = [...password].filter((x) => x === letter).length;
-  return letter_count >= a && letter_count <= b;
+    const [, min, max, letter, password] = parse_line(input);
+    const letter_count = [...password].filter(x => x === letter).length;
+    return letter_count >= min && letter_count <= max;
 }
 
 function validate_password2(input) {
-  const [_, a, b, letter, password] = parse_line(input);
-  return (password[a - 1] === letter) ^ (password[b - 1] === letter);
+    const [, a, b, letter, password] = parse_line(input);
+    return (password[a - 1] === letter) ^ (password[b - 1] === letter);
 }
 
 function part1(lines) {
-  let result = 0;
-  lines.forEach((x) => {
-    if (validate_password(x)) result += 1;
-  });
-  console.log(result);
+    let result = lines.filter(x => validate_password(x)).length;
+    console.log(result);
 }
 
 function part2(lines) {
-  let result = 0;
-  lines.forEach((x) => {
-    if (validate_password2(x)) result += 1;
-  });
-  console.log(result);
+    let result = lines.filter(x => validate_password2(x)).length;
+    console.log(result);
 }
